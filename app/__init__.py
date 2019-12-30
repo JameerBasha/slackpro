5e4fa63bd5770cad9185faf8bbe9e270dcf9a3a8
Jameerbasha J <jameerbasha14@gmail.com>
2019-12-30 18:42:11 +0000

    change the environment variable values of elasticsearch

--- app/__init__.py
+++ app/__init__.py
@@ -29,8 +29,8 @@ migrate.init_app(app,db)
 moment.init_app(app)
 celery=Celery(app.name,broker='redis://localhost:6379',backend='redis://localhost:6379')
 celery.conf.update(app.config)
-auth = ('elastic', 'JyvM68PAVaRSa1c4UM9NZvM7')
-elasticsearch=Elasticsearch("http://19d6d3ac74d34f8fb6ed9b7743c2296b.ap-southeast-1.aws.found.io:9243/", http_auth=auth, use_ssl=True, verify_certs=False)
+auth = ('elastic', 'mV07XldWSt8ijrHKO8zxv4u2')
+elasticsearch=Elasticsearch("http://d293ac223fad4eb48460c51fa6a862ec.ap-southeast-1.aws.found.io:9243/", http_auth=auth, use_ssl=True, verify_certs=False)
 
 #elasticsearch=Elasticsearch([app.config['ELASTICSEARCH_URL']])\
 	# if app.config['ELASTICSEARCH_URL'] else None
